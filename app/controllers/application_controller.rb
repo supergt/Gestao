b881a6f49ec1c410cea01e5c38baa61dcd7eca93
Renata Wai <renatayuki0@gmail.com>
2012-06-12 22:30:35 -0300

    Modal inicial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  layout Proc.new { |controller| controller.request.xhr? ? nil : 'application' }
 end
--- vendor/assets/javascripts/myapp.js
+++ vendor/assets/javascripts/myapp.js
@@ -1,47 +1,25 @@
-﻿$(document).ready(function() {  
- 
-    //select all the a tag with name equal to modal
-    $('a[name=modal]').click(function(e) {
-        //Cancel the link behavior
-        e.preventDefault();
-        //Get the A tag
-        var id = $(this).attr('href');
-     
-        //Get the screen height and width
-        var maskHeight = $(document).height();
-        var maskWidth = $(window).width();
-     
-        //Set height and width to mask to fill up the whole screen
-        $('#mask').css({'width':maskWidth,'height':maskHeight});
-         
-        //transition effect     
-        $('#mask').fadeIn(1000);    
-        $('#mask').fadeTo("slow",0.8);  
-     
-        //Get the window height and width
-        var winH = $(window).height();
-        var winW = $(window).width();
-               
-        //Set the popup window to center
-        $(id).css('top',  winH/2-$(id).height()/2);
-        $(id).css('left', winW/2-$(id).width()/2);
-     
-        //transition effect
-        $(id).fadeIn(2000); 
-     
-    });
-     
-    //if close button is clicked
-    $('.window .close').click(function (e) {
-        //Cancel the link behavior
-        e.preventDefault();
-        $('#mask, .window').hide();
-    });     
-     
-    //if mask is clicked
-    $('#mask').click(function () {
-        $(this).hide();
-        $('.window').hide();
-    });         
-     
+﻿$(function(){
+  var $modal = $('#modal'),
+      $modal_close = $modal.find('.close'),
+      $modal_container = $('#modal-container');
+
+
+  $('a[data-remote]').live('ajax:beforeSend', function(e, xhr, settings){
+    xhr.setRequestHeader('accept', '*/*;q=0.5, text/html, ' + settings.accepts.html);
+  });
+
+  $('a[data-remote]').live('ajax:success', function(xhr, data, status){
+    $modal
+      .html(data)
+      .prepend($modal_close)
+      .css('top', $(window).scrollTop() + 40)
+      .show();
+    $modal_container.show();
+  });
+
+  $('.close', '#modal').live('click', function(){
+    $modal_container.hide();
+    $modal.hide();
+    return false;
+  });
 });
